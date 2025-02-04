a810a15e42652afcbf7dd4affd345ee303b2082d
Karennewton12 <newtonkaren782@gmail.com>
2025-02-04 13:10:52 -0600

    xcdfg

--- script.js
+++ script.js
@@ -1,37 +1,53 @@
-let paragraph = document.createElement('p');
-        paragraph.textContent = "0"
-        let change = document.getElementsByTagName("div");
-        let changeEvent = change[0]
-        changeEvent.appendChild(paragraph)
-        console.log(changeEvent)
-        paragraph.style.backgroundColor = "red";
-        paragraph.style.color = "white";
-        paragraph.style.textAlign = "center";
-        paragraph.style.width = "300px";
-        paragraph.style.height = "300px";
-        paragraph.style.alignContent = "center";
-        paragraph.style.fontSize = "5rem";
-        paragraph.style.border = "3px solid black";
-        paragraph.style.borderRadius = "150px";
-        paragraph.style.cursor= "pointer";
+        // let paragraph = document.createElement('p');
+        // paragraph.textContent = "0"
+        // let change = document.getElementsByTagName("div");
+        // let changeEvent = change[0]
+        // changeEvent.appendChild(paragraph)
+        // console.log(changeEvent)
+        // paragraph.style.backgroundColor = "red";
+        // paragraph.style.color = "white";
+        // paragraph.style.textAlign = "center";
+        // paragraph.style.width = "300px";
+        // paragraph.style.height = "300px";
+        // paragraph.style.alignContent = "center";
+        // paragraph.style.fontSize = "5rem";
+        // paragraph.style.border = "3px solid black";
+        // paragraph.style.borderRadius = "150px";
+        // paragraph.style.cursor= "pointer";
 
 
-        const increaseBtn = document.getElementById("increaseBtn");
-        const resetBtn = document.getElementById("resetBtn");
-        const decreaseBtn = document.getElementById("decreaseBtn");
-        const Counting = document.getElementsByTagName("div");
-        let count = 0;
+        // const increaseBtn = document.getElementById("increaseBtn");
+        // const resetBtn = document.getElementById("resetBtn");
+        // const decreaseBtn = document.getElementById("decreaseBtn");
+        // const Counting = document.getElementsByTagName("div");
+        // let count = 0;
     
         
-        resetBtn.addEventListener("click", () => {
-            count = 0;
-            Counting[0].textContent = count;
-        });
-         increaseBtn.addEventListener("click", () => {
-             count++;
-             Counting[0].textContent = count;
-         });
-        decreaseBtn.addEventListener("click", () => {
-            count--;
-            Counting[0].textContent = count;
-        });
+        // resetBtn.addEventListener("click", () => {
+        //     count = 0;
+        //     Counting[0].textContent = count;
+        // });
+        //  increaseBtn.addEventListener("click", () => {
+        //      count++;
+        //      Counting[0].textContent = count;
+        //  });
+        // decreaseBtn.addEventListener("click", () => {
+        //     count--;
+        //     Counting[0].textContent = count;
+        // });
+
+         const text = document.getElementById("right");
+
+         text.addEventListener("click", () => {
+            document.body.style.border ="2px solid black"
+            document.body.style.backgroundColor = "red";
+            document.body.style.color = "white";
+            document.body.style.textAlign = "center";
+            document.body.style.width = "300px";
+            document.body.height = "300px";
+            document.body.style.alignContent = "center";
+            document.body.style.fontSize = "5rem";
+            document.body.style.border = "3px solid black";
+            document.body.borderRadius = "150px";
+            document.body.style.cursor= "pointer";
+         }
